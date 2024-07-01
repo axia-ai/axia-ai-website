a4b68b9e178886a0aef2f94c7d943a354d112ce1
Siddharth Khonde <sidconstructs@gmail.com>
2024-07-01 19:06:18 -0400

    updated sub-heading content

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -8,9 +8,9 @@ const HeroSection = () => {
     <div className="relative bg-cover bg-center" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="flex flex-col items-center justify-center h-full bg-black bg-opacity-5 text-white px-8 py-8 lg:py-20">
         <h1 className="text-3xl lg:text-5xl font-bold text-introHeadingBlack">Ready to Revolutionize Your Business with AI?</h1>
-        <h1 className="text-3xl lg:text-5xl font-bold text-introHeadingBlack">Re-Imagine your business with innovative AI strategies and solutions by harnessing the latest in AI Technology to drive growth, efficiency, and scalability.</h1>
+        <h1 className="text-3xl lg:text-5xl font-bold text-introHeadingBlack"></h1>
         <p className="mt-4 text-sm lg:text-lg text-center max-w-2xl text-introHeadingBlack">
-          At lacus vitae nulla sagittis scelerisque nisl. Pellentesque duis cursus vestibulum, facilisi ac, sed faucibus.
+          Re-Imagine your business with innovative AI strategies and solutions by harnessing the latest in AI Technology to drive growth, efficiency, and scalability.
         </p>
         <button className="mt-6 px-4 py-2 lg:px-6 lg:py-3 bg-introHeadingBlack text-white font-semibold rounded-md hover:bg-gray-800">
           Talk To Us

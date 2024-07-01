9a82b35b70e32d60ed264636d69495cd8443c3d6
Siddharth Khonde <sidconstructs@gmail.com>
2024-07-01 19:00:17 -0400

    updated hero section content

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -7,8 +7,8 @@ const HeroSection = () => {
   return (
     <div className="relative bg-cover bg-center" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="flex flex-col items-center justify-center h-full bg-black bg-opacity-5 text-white px-8 py-8 lg:py-20">
-        <h1 className="text-3xl lg:text-5xl font-bold text-introHeadingBlack">Suspendisse vitae</h1>
-        <h1 className="text-3xl lg:text-5xl font-bold text-introHeadingBlack">pharetra netus</h1>
+        <h1 className="text-3xl lg:text-5xl font-bold text-introHeadingBlack">Ready to Revolutionize Your Business with AI?</h1>
+        <h1 className="text-3xl lg:text-5xl font-bold text-introHeadingBlack">Re-Imagine your business with innovative AI strategies and solutions by harnessing the latest in AI Technology to drive growth, efficiency, and scalability.</h1>
         <p className="mt-4 text-sm lg:text-lg text-center max-w-2xl text-introHeadingBlack">
           At lacus vitae nulla sagittis scelerisque nisl. Pellentesque duis cursus vestibulum, facilisi ac, sed faucibus.
         </p>

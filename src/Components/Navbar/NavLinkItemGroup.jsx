7ac1bbcca344057c397d5f32946fa987a50edd24
omkar342 <omkarjadhav095@gmail.com>
2024-07-19 19:31:52 +0530

    removed typo in navbar list item.

--- src/Components/Navbar/NavLinkItemGroup.jsx
+++ src/Components/Navbar/NavLinkItemGroup.jsx
@@ -5,7 +5,7 @@ const NavLinkItemGroup = () => (
     <>
         <NavLinkItem name="Home" to="home" />
         <NavLinkItem name="Services" to="services" />
-        <NavLinkItem name="Industries" to="ndustries" />
+        <NavLinkItem name="Industries" to="industries" />
         <NavLinkItem name="Testimonials" to="testimonials" />
         <NavLinkItem name="About Us" to="aboutUs" />
         <NavLinkItem name="Contact Us" to="contactUs" />

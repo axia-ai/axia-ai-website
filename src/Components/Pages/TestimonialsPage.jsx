25da9e4f36a4f9edc28f6c4976956e7cf9115379
omkar342 <omkarjadhav095@gmail.com>
2024-07-24 13:56:19 +0530

    testimonial page is added.

--- src/Components/Pages/TestimonialsPage.jsx
+++ src/Components/Pages/TestimonialsPage.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import Layout from '../Layout/Layout'
+import React from "react";
+import Layout from "../Layout/Layout";
+import TestimonialPage from "../Testimonials/TestimonialPage";
 
 function Testimonials() {
   return (
-    <Layout heroHeading={"Testimonials"} heroPara={"The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 's"}>Testimonials</Layout>
-  )
+    <Layout
+      heroHeading={"Testimonials"}
+      heroPara={
+        "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 's"
+      }
+    >
+      <TestimonialPage />
+    </Layout>
+  );
 }
 
-export default Testimonials
+export default Testimonials;

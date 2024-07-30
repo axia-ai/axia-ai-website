dfaf0e08352c9c02f29140aae9a1e29eab09bce9
sidconstructs <siddharth@ax-ia.ai>
2024-07-30 19:48:22 -0400

    update content for About Us section

--- src/Components/Pages/AboutUsPage.jsx
+++ src/Components/Pages/AboutUsPage.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import Layout from '../Layout/Layout'
 import AboutUsVision from '../AboutUsSection/AboutUsVision'
 import AboutUsStory from '../AboutUsSection/AboutUsStories'
-import AboutUsInfo from '../AboutUsSection/AboutUsInfo'
+// import AboutUsInfo from '../AboutUsSection/AboutUsInfo'
 
 function AboutUs() {
   return (
-    <Layout heroHeading={"About Us"} heroPara={"The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 's"}>
-      <AboutUsStory />
+    <Layout heroHeading={"Our Story"} heroPara={"We help you to unleash the power of AI to transform your business."}>
       <AboutUsVision />
-      <AboutUsInfo/>
+      <AboutUsStory />
+      {/* <AboutUsInfo /> */}
+
     </Layout>
   )
 }

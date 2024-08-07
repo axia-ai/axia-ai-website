6225d454a67eb103c7370fa77690b5734db2ffe9
omkar342 <omkarjadhav095@gmail.com>
2024-08-07 16:06:43 +0530

    placeholders added to contactus form.

---
+++ src/JsonData/domain.js
@@ -0,0 +1,7 @@
+const domainArray = [
+    { value: "SF", label: "Software" },
+    { value: "AI", label: "Artificial Intelligence" },
+    { value: "DB", label: "Database" },
+  ];
+  
+  export default domainArray;

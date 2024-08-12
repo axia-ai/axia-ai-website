93ed91369f95d4eca46a8ef2590020a1c06f0146
sohamtapse <tapsesoham0@gmail.com>
2024-08-12 13:51:47 +0530

    OurServices page added in aboutus section

--- src/Components/Pages/AboutUsPage.jsx
+++ src/Components/Pages/AboutUsPage.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import Layout from '../Layout/Layout'
-import AboutUsVision from '../AboutUsSection/AboutUsVision'
-import AboutUsStory from '../AboutUsSection/AboutUsStories'
-import AboutUsInfo from '../AboutUsSection/AboutUsInfo'
-import AboutUsValues from '../AboutUsSection/AboutUsValues'
-import Teams from '../Teams/Teams'
+import React from "react";
+import Layout from "../Layout/Layout";
+import AboutUsValues from "../AboutUsSection/AboutUsValues";
+import Teams from "../Teams/Teams";
+import Our_Services from "../AboutUsSection/Our_Services";
 
 function AboutUs() {
   return (
-    <Layout heroHeading={"Our Story"} heroPara={"We help you to unleash the power of AI to transform your business."}>
-      {/* <AboutUsVision /> 
-      <AboutUsStory /> 
-      <AboutUsInfo /> */}
-      <AboutUsValues/>
-      <Teams/>
+    <Layout
+      heroHeading={"Our Story"}
+      heroPara={
+        "We help you to unleash the power of AI to transform your business."
+      }
+    >
+      <AboutUsValues />
+      <Our_Services />
+      <Teams />
     </Layout>
-  )
+  );
 }
 
-export default AboutUs
+export default AboutUs;

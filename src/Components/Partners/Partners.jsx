d563d374c6a54858078c3a3c3cd6a6925108e961
sohamtapse <tapsesoham0@gmail.com>
2024-09-18 15:26:59 +0530

    Our Partners component done in loop.

--- src/Components/Partners/Partners.jsx
+++ src/Components/Partners/Partners.jsx
@@ -1,49 +1,57 @@
-import React from 'react'
-import { FaAirbnb } from "react-icons/fa";
-import { FaAmazon } from "react-icons/fa";
-import { FaMicrosoft } from "react-icons/fa";
-import { FaFedex } from "react-icons/fa";
-import { FaHubspot } from "react-icons/fa";
+import React from "react";
+import Marquee from "react-fast-marquee";
+import image1 from "../../assets/TestimonialsBlackWhiteLogos/1.png";
+import image2 from "../../assets/TestimonialsBlackWhiteLogos/11.png";
+import image3 from "../../assets/TestimonialsBlackWhiteLogos/3.png";
+import image4 from "../../assets/TestimonialsBlackWhiteLogos/4.png";
+import image5 from "../../assets/TestimonialsBlackWhiteLogos/5.png";
+import image6 from "../../assets/TestimonialsBlackWhiteLogos/6.png";
+import image7 from "../../assets/TestimonialsBlackWhiteLogos/7.png";
+import image8 from "../../assets/TestimonialsBlackWhiteLogos/8.png";
 
-const Partners = () =>{
-    return(
-        <>
-            <div className="flex flex-col">
-                <div className='flex justify-center items-center  w-auto m-7 max-md:m-3'>
-                    <h1 className='font-poppins font-bold text-5xl max-md:text-2xl'>Our Partners</h1>
-                </div>
-                <hr className='mb-4 mx-4 ' />
-                <div className='w-full flex  justify-center items-center mx-0  max-md:justify-between max-md:items-center '>
-                    <div className='flex justify-between items-center w-auto max-md:mx-2'>
-                        <div className='flex justify-center mx-10 items-center max-md:mx-1'>
-                            <FaAirbnb className='size-11 max-md:size-4' />
-                            <p className='font-poppins font-bold text-2xl mx-1 max-md:text-xs'>airbnb</p>
-                        </div>
-                        <div className='flex justify-center mx-10 items-center max-md:mx-1'>
-                            <p className='font-poppins font-bold text-3xl max-md:text-xs '>Google</p>
-                        </div>
-                        <div className='flex justify-center mx-10 items-center max-md:mx-1'>
-                            <FaAmazon className='size-10 max-md:size-4' />
-                            <p className='font-poppins font-bold text-2xl mx-1 max-md:text-xs'>amazon</p>
-                        </div>
-                        <div className='flex justify-center mx-10 items-center max-md:mx-1'>
-                            <FaMicrosoft className='size-11 max-md:size-4' />
-                            <p className='font-poppins font-bold text-xl text-gray-400 mx-1 max-md:text-xs'>Microsoft</p>
-                        </div>
-                        <div className='flex justify-center mx-10  items-center max-md:mx-1'>
-                            <FaFedex className='size-24 max-md:size-8' />
-                        </div>
-                        <div className='flex justify-center mx-10  items-center max-md:mx-1'>
-                            <FaHubspot className='size-11 max-md:size-4' />
-                            <p className='font-poppins font-bold text-2xl mx-1 max-md:text-xs'>Hubspot</p>
-                        </div>
-                    </div>
-
-                </div>
-                <hr className='m-4'  />
+const Partners = () => {
+  return (
+    <>
+      <div className="flex flex-col">
+        <div className="flex justify-center items-center  w-auto m-7 max-md:m-3">
+          <h1 className="font-poppins font-bold text-6xl max-md:text-3xl">
+            Our Partners
+          </h1>
+        </div>
+        <hr className="mb-4 mx-4 " />
+        <Marquee loop={0} speed={100}>
+          <div className="flex justify-between items-center w-auto max-md:mx-2 ">
+            <div className="flex justify-center mx-1 items-center max-md:mx-1 size-60 ">
+              <img src={image1} alt="" />
             </div>
-        </>
-    );
+            <div className="flex justify-center mx-1 items-center max-md:mx-1 size-60 ">
+              <img src={image2} alt="" />
+            </div>
+            <div className="flex justify-center mx-1 items-center max-md:mx-1 size-60">
+              <img src={image3} alt="" />
+            </div>
+            <div className="flex justify-center mx-1 items-center max-md:mx-1 size-60">
+              <img src={image4} alt="" />
+            </div>
+            <div className="flex justify-center mx-1  items-center max-md:mx-1 size-60">
+              <img src={image5} alt="" />
+            </div>
+            <div className="flex justify-center mx-1  items-center max-md:mx-1 size-60">
+              <img src={image6} alt="" />
+            </div>
+            <div className="flex justify-center mx-1  items-center max-md:mx-1 size-60">
+              <img src={image7} alt="" />
+            </div>
+            <div className="flex justify-center mx-1  items-center max-md:mx-1 size-60">
+              <img src={image8} alt="" />
+            </div>
+          </div>
+        </Marquee>
+
+        <hr className="m-4" />
+      </div>
+    </>
+  );
 };
 
 export default Partners;

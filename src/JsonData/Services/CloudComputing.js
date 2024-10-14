c4797a8bd9890d6b1b53ca5f801eaae7fda48cd0
omkar342 <omkarjadhav095@gmail.com>
2024-10-14 11:13:27 +0530

    icons added in sub services.

--- src/JsonData/Services/CloudComputing.js
+++ src/JsonData/Services/CloudComputing.js
@@ -1,17 +1,22 @@
 // data/industryCards.js
 import { GoDatabase } from "react-icons/go";
+import { LiaMixcloud } from "react-icons/lia";
+import { AiOutlineDatabase } from "react-icons/ai";
+import { CgWebsite } from "react-icons/cg";
+import { FiDatabase } from "react-icons/fi";
+import { MdOutlineSecurity } from "react-icons/md";
 
 export const FirstDeck = [
   {
     title: "Cloud Migration and Optimization",
     description: "We assist clients in seamlessly migrating their existing applications and data to cloud platforms. Optimize cloud infrastructure for performance, cost-efficiency, and scalability, ensuring a smooth transition to the cloud.",
-    icon: GoDatabase,
+    icon: LiaMixcloud,
     linkAddress: ""
   },
   {
     title: "Infrastructure as a Service (IaaS)",
     description: "We provide on-demand access to fundamental computing resources, including servers, storage, and networking. This allows businesses to scale resources up or down as needed, eliminating the need for upfront investments in hardware.",
-    icon: GoDatabase,
+    icon: AiOutlineDatabase,
     linkAddress: ""
   },
   {
@@ -23,19 +28,19 @@ export const FirstDeck = [
   {
     title: "Software as a Service (SaaS)",
     description: "We deliver pre-built software applications over the internet, eliminating the need for local installation and maintenance. SaaS solutions can range from productivity tools to enterprise resource planning (ERP) systems.",
-    icon: GoDatabase,
+    icon: CgWebsite,
     linkAddress: ""
   },
   {
     title: "Big Data Analytics",
     description: "We utilize cloud-based platforms to analyze large datasets and extract valuable insights. Our data engineers can help clients leverage big data analytics to make informed decisions and gain a competitive advantage.",
-    icon: GoDatabase,
+    icon: FiDatabase,
     linkAddress: ""
   },
   {
     title: "Cloud Security and Compliance",
     description: "Ensure data security, privacy, and compliance with industry regulations in cloud environments. We implement robust security measures, conduct regular audits, and provide expert guidance on data protection best practices.",
-    icon: GoDatabase,
+    icon: MdOutlineSecurity,
     linkAddress: ""
   }
 ];
--- src/JsonData/Services/IOT.js
+++ src/JsonData/Services/IOT.js
@@ -1,41 +1,46 @@
 // data/industryCards.js
-import { GoDatabase } from "react-icons/go";
+import { MdOutlineSecurity } from "react-icons/md";
+import { MdOutlineMiscellaneousServices } from "react-icons/md";
+import { TbSettingsAutomation } from "react-icons/tb";
+import { FaPerson } from "react-icons/fa6";
+import { BsDatabase } from "react-icons/bs";
+import { SiGooglecontaineroptimizedos } from "react-icons/si";
 
 export const FirstDeck = [
   {
     title: "Data Analytics and Insights",
     description: "We leverage AI to analyze vast amounts of IoT data, extracting valuable insights and identifying trends that can inform decision-making and optimize operations, ultimately driving business growth, innovation, and competitive advantage.",
-    icon: GoDatabase,
+    icon: BsDatabase,
     linkAddress: ""
   },
   {
     title: "Predictive Maintenance",
     description: "We utilize AI algorithms to predict equipment failures and schedule maintenance proactively, reducing downtime and costs. AI can analyze sensor data, historical records, and other factors to identify potential issues beforehand.",
-    icon: GoDatabase,
+    icon: MdOutlineMiscellaneousServices,
     linkAddress: ""
   },
   {
     title: "Intelligent Automation",
     description: "We automate tasks and processes using AI to improve efficiency, reduce manual errors, and optimize resource allocation. AI-powered automation can streamline operations, freeing up resources for more strategic initiatives.",
-    icon: GoDatabase,
+    icon: TbSettingsAutomation,
     linkAddress: ""
   },
   {
     title: "Personalized Experiences",
     description: "We tailor IoT experiences to individual users based on their preferences and behavior. AI can analyze user data to provide personalized recommendations, notifications, and content, enhancing user satisfaction and engagement.",
-    icon: GoDatabase,
+    icon: FaPerson,
     linkAddress: ""
   },
   {
     title: "Security and Anomaly Detection",
     description: "Implement AI-powered security solutions to detect and prevent security threats in IoT networks. AI can analyze network traffic, sensor data, and user behavior to identify anomalies and respond to potential attacks.",
-    icon: GoDatabase,
+    icon: MdOutlineSecurity,
     linkAddress: ""
   },
   {
     title: "Optimization and Efficiency",
     description: "Use AI to optimize IoT systems for energy efficiency, resource allocation, and performance. AI can analyze data to identify areas for improvement and optimize resource utilization, reducing costs and improving sustainability.",
-    icon: GoDatabase,
+    icon: SiGooglecontaineroptimizedos,
     linkAddress: ""
   }
 ];

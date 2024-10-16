4fc444e817f58e2780b1f91461355f37bcfc27a2
omkar342 <omkarjadhav095@gmail.com>
2024-10-16 10:12:04 +0530

    bug in pink background image is resolved.

--- src/Components/ReusableComponents/PinkBackgroundComponent.jsx
+++ src/Components/ReusableComponents/PinkBackgroundComponent.jsx
@@ -3,9 +3,9 @@ import backgroundVectorImage from '../../assets/background_vector.png';
 
 function PinkBackgroundComponent({ children }) {
     return (
-        <div className="flex flex-col items-center bg-gray-100 min-h-screen p-6">
+        <div className="flex flex-col items-center bg-gray-100">
             <div
-                className="bg-center bg-no-repeat bg-[length:0%] sm:bg-[length:0%] md:bg-[length:75%] bg-none sm:bg-cover"
+                className="bg-center bg-no-repeat bg-[length:0%] sm:bg-[length:0%] md:bg-[length:100%] bg-none sm:bg-cover"
                 style={{ backgroundImage: `url(${backgroundVectorImage})` }}
             >
                 {children}
